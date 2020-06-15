c370e95b3266758ad83334a0477cc91edcab78dc
Sandesh Dinkar <43902225+SldinkarGECA@users.noreply.github.com>
2020-06-15 12:58:36 +0530

    added tie condition

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ const itemArray = new Array(9).fill("empty");
 
 const App = () => {
   const [isCross, setIsCross] = useState(false);
-
+  const [tie, setTie] = useState(0);
   const [winMessage, setWinMessage] = useState("");
 
   const reloadGame = () => {
     setIsCross(false);
+    setTie(0);
     setWinMessage("");
     itemArray.fill("empty", 0, 9);
   };
@@ -34,57 +35,61 @@ const App = () => {
       itemArray[3] !== "empty"
     ) {
       setWinMessage(`${itemArray[3]} wins`);
-    }else if (
-        itemArray[6] === itemArray[7] &&
-        itemArray[6] === itemArray[8] &&
-        itemArray[6] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[6]} wins`);
-      }else if (
-        itemArray[0] === itemArray[3] &&
-        itemArray[0] === itemArray[6] &&
-        itemArray[0] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[0]} wins`);
-      }else if (
-        itemArray[1] === itemArray[4] &&
-        itemArray[1] === itemArray[7] &&
-        itemArray[1] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[1]} wins`);
-      }else if (
-        itemArray[2] === itemArray[5] &&
-        itemArray[2] === itemArray[8] &&
-        itemArray[2] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[2]} wins`);
-      }else if (
-        itemArray[0] === itemArray[4] &&
-        itemArray[0] === itemArray[8] &&
-        itemArray[0] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[0]} wins`);
-      }else if (
-        itemArray[2] === itemArray[4] &&
-        itemArray[2] === itemArray[6] &&
-        itemArray[2] !== "empty"
-      ) {
-        setWinMessage(`${itemArray[2]} wins`);
-      }
+    } else if (
+      itemArray[6] === itemArray[7] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[6] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (
+      itemArray[0] === itemArray[3] &&
+      itemArray[0] === itemArray[6] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7] &&
+      itemArray[1] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (
+      itemArray[2] === itemArray[5] &&
+      itemArray[2] === itemArray[8] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    } else if (
+      itemArray[0] === itemArray[4] &&
+      itemArray[0] === itemArray[8] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[2] === itemArray[4] &&
+      itemArray[2] === itemArray[6] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    }
   };
 
   const changeItem = (itemNumber) => {
     if (winMessage) {
-      return toast(winMessage, {type: "success"});
+      return toast(winMessage, { type: "success" });
     }
     if (itemArray[itemNumber] === "empty") {
       itemArray[itemNumber] = isCross ? "cross" : "circle";
       setIsCross(!isCross);
+      setTie(tie + 1);
     } else {
-      return toast("Already filled", {type: "error"});
+      return toast("Already filled", { type: "error" });
     }
 
     checkIsWinner();
+    if (tie === 8) {
+      setWinMessage("Match Tied");
+    }
   };
 
   return (
@@ -92,20 +97,20 @@ const App = () => {
       <ToastContainer position="bottom-center" />
       <Row>
         <Col md={6} className="offset-md-3">
-        {winMessage ? (
-        <div className="mb-2 mt-2">
-          <h1 className="text-success text-uppercase text-center">
-            {winMessage}
-          </h1>
-          <Button color="success" block onClick={reloadGame}>
-            START NEW GAME
-          </Button>
-        </div>
-      ) : (
-        <h1 className="text-warning text-uppercase text-center">
-          {isCross ? "Cross" : "Circle"}'s turn
-        </h1>
-      )}
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-success text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>
+                START NEW GAME
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-warning text-uppercase text-center">
+              {isCross ? "Cross" : "Circle"}'s turn
+            </h1>
+          )}
           <div className="grid">
             {itemArray.map((item, index) => (
               <Card
